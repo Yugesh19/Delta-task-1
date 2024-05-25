c82b207167274d5abdaf5fbbc404288ccd71ced5
Yugesh19 <yugesh.rps@gmail.com>
2024-05-25 23:49:48 +0530

    Added boolean attribute for each piece and added event listener to each square

--- main.js
+++ main.js
@@ -2,38 +2,45 @@ const cols = {0:"A", 1:"B", 2:"C", 3:"D", 4:"E", 5:"F", 6:"G", 7:"H"}
 const table = document.createElement("table");
 table.className = "board";
 let a = 1;
+let coords = []
 for (let i = 1; i < 9; i++) {
     let tr = document.createElement('tr');
     tr.dataset.line = i
     for (let j = 1; j < 9; j++) {
         let td = document.createElement('td');
-        td.setAttribute("id",("box"+String(a)))
-        let d = document.createElement('div');
-        d.className = (("box"+String(a)))
-        td.appendChild(d);
-        td.dataset.col = cols[j-1];
-        td.dataset.line = i;
+
+        /*td.setAttribute("id",("box"+String(a)))*/
+
+        td.dataset.row = i;
+        td.dataset.col = j;
+        c=[i,j];
+        coords.push(c);
+
+        td.dataset.tank = false;
+        td.dataset.rico = false;
+        td.dataset.srico = false;
+        td.dataset.titan = false;
+
+        td.addEventListener("click", function (e) {highlight(this)})
+
         td.className = (i%2 === j%2) ? "white square" : "black square";
-        /*td.classList.add(("box"+String(a)));*/
+        td.classList.add(("box"+String(a)));
         tr.appendChild(td);
         a++;
     }
     table.appendChild(tr);
 }
 document.querySelector("div").appendChild(table);
+/*console.log(coords);*/
 
+ball = document.querySelector(".box1");
+console.log(ball.getAttribute("data-tank"))
+ball.setAttribute("data-tank",true)
 
-var color = [, "#3C9EE7", "#E7993C",
-            "#E73C99", "#3CE746", "#E7993C"];
-box1 = document.querySelectorAll(".square")
-box1.forEach(Square => {
-    Square.addEventListener("click", function (){
-        for(let i = 1;i<=64;i++){
-            p = "box"+String(i);
-            document.getElementById(p).style.background = color[(Math.floor(Math.random() * color.length))];
-        }
+function highlight(s){
+    if((s.getAttribute("data-tank")==="true")||(s.getAttribute("data-titan")==="true")||(s.getAttribute("data-rico")==="true")||(s.getAttribute("data-srico")==="true")){
+        let row = s.getAttribute("data-row")
+        let col = s.getAttribute("data-col")
+        console.log(row,col)
     }
-    );
-    
-})
-
+}
