55c1094690abe23b89d2a37e88bd617e69f6246f
Yugesh19 <yugesh.rps@gmail.com>
2024-05-26 21:10:37 +0530

    Set positions of pieces randomly fixed highlighting on clicking multiple pieces styled pieces

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const table = document.createElement("table");
 table.className = "board";
 let a = 1;
 let coords = [];
+let currentplayer = 1;
 
 boardCreator()
 
@@ -17,6 +18,7 @@ function boardCreator(){
             let c = [i, j];
             coords.push(c);
 
+            td.dataset.player = 0;
             td.dataset.tank = false;
             td.dataset.rico = false;
             td.dataset.srico = false;
@@ -38,14 +40,71 @@ function boardCreator(){
 }
 
 document.querySelector("div").appendChild(table);
+setInitialPieces();
+
+/*start*/
+
+function setInitialPieces() {
+    // Cannons at fixed positions
+    setPiece(1, 1, 'cannon', 1);  // Player 1 cannon at bottom left
+    setPiece(8, 8, 'cannon', 2);  // Player 2 cannon at top right
+
+    // Randomly place other pieces for Player 1
+    let player1Pieces = ['tank', 'rico', 'srico', 'titan'];
+    player1Pieces.forEach(piece => placeRandomPiece(piece, 1));
+
+    // Randomly place other pieces for Player 2
+    let player2Pieces = ['tank', 'rico', 'srico', 'titan'];
+    player2Pieces.forEach(piece => placeRandomPiece(piece, 2));
+}
+
+function placeRandomPiece(piece, player) {
+    let row, col, cell;
+    do {
+        row = getRandomInt(1, 8);
+        col = getRandomInt(1, 8);
+        cell = document.querySelector(`td[data-row='${row}'][data-col='${col}']`);
+    } while (cell.getAttribute("data-player") != 0 || (row === 1 && col === 1) || (row === 8 && col === 8));
+
+    setPiece(row, col, piece, player);
+}
+
+function setPiece(row, col, piece, player) {
+    let cell = document.querySelector(`td[data-row='${row}'][data-col='${col}']`);
+    cell.setAttribute(`data-${piece}`, true);
+    cell.setAttribute("data-player", player);
+    cell.classList.add(`player${player}-${piece}`);
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+/*stop*/
+
+
 
 let ball = document.querySelector(".box1");
 ball.setAttribute("data-tank", true);
 
 document.querySelector(".box62").setAttribute("data-cannon",true);
 
+
+
 function checkPiece(s) {
-    if (s.getAttribute("data-tank") === "true" || s.getAttribute("data-titan") === "true" || s.getAttribute("data-rico") === "true" || s.getAttribute("data-srico") === "true"|| s.getAttribute("data-cannon") === "true") {
+    check = (s.getAttribute("data-tank") === "true" || s.getAttribute("data-titan") === "true" || s.getAttribute("data-rico") === "true" || s.getAttribute("data-srico") === "true"|| s.getAttribute("data-cannon") === "true")
+
+    if((s.getAttribute("data-highlight") === "true")&&(!check)){
+        console.log("hello")
+    }
+
+    if (check) {
+        unhigh = document.getElementsByClassName("square")
+        Array.from(unhigh).forEach(element => element.setAttribute("data-highlight",false))
+        
         let row = Number(s.getAttribute("data-row"));
         let col = Number(s.getAttribute("data-col"));
         if(s.getAttribute("data-cannon") !== "true"){
@@ -72,4 +131,5 @@ function checkPiece(s) {
         unhigh = document.getElementsByClassName("square")
         Array.from(unhigh).forEach(element => element.setAttribute("data-highlight",false))
     }
+    
 }
