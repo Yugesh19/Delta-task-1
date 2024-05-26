eae2a62b3b1b0fd04d7e8c509911d246b0081377
Yugesh19 <yugesh.rps@gmail.com>
2024-05-26 18:15:37 +0530

    Added array to contain adjacent boxes

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ for (let i = 1; i < 9; i++) {
         td.dataset.srico = false;
         td.dataset.titan = false;
 
-        td.addEventListener("click", function (e) {highlight(this)})
+        td.addEventListener("click", function (e) {checkPiece(this)})
 
         td.className = (i%2 === j%2) ? "white square" : "black square";
         td.classList.add(("box"+String(a)));
@@ -30,17 +30,55 @@ for (let i = 1; i < 9; i++) {
     }
     table.appendChild(tr);
 }
+
 document.querySelector("div").appendChild(table);
 /*console.log(coords);*/
 
 ball = document.querySelector(".box1");
-console.log(ball.getAttribute("data-tank"))
 ball.setAttribute("data-tank",true)
 
-function highlight(s){
+
+
+function checkPiece(s){
     if((s.getAttribute("data-tank")==="true")||(s.getAttribute("data-titan")==="true")||(s.getAttribute("data-rico")==="true")||(s.getAttribute("data-srico")==="true")){
-        let row = s.getAttribute("data-row")
-        let col = s.getAttribute("data-col")
-        console.log(row,col)
+        let row = s.getAttribute("data-row");
+        let col = s.getAttribute("data-col");
+        ro=Number(row);
+        co=Number(col);
+        let index = getIndex(row,col)
+        
+        let ul = [ro-1,co-1]
+        let u =  [ro-1,co]
+        let ur = [ro-1,co+1]
+        let r = [ro,co+1]
+        let dr = [ro+1,co+1]
+        let d = [ro+1,co]
+        let dl = [ro+1,co-1]
+        let l = [ro,co-1]
+        h = [ul,u,ur,r,dr,d,dl,l]
+        
+        for(let i=0;i<8;i++){
+            let ind;
+            ind = getIndex(h[i][0],h[i][1])
+            viab(ind,i)
+        }
+        
+        
+    }
+}
+
+function getIndex(r,c){
+    for(let i=0;i<64;i++){
+        if(coords[i][0]===Number(r)){
+            if(coords[i][1]===Number(c)){
+                return [+r,+c]
+            }
+        }
+    }
+}
+
+function viab(ind,i){
+    if(typeof ind === "undefined"){
+        console.log(h[i])
     }
 }
